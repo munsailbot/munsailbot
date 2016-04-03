1b080c75d288e5a0b6c9da48b76628f5bdb2343c
bnuh <bryan@okplus.ca>
2016-04-03 16:22:47 -0230

    Adding build folder

---
+++ BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/ArduinoEncoder.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/ArduinoEncoder.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/Autonomy.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/Autonomy.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/AverageAngle.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/AverageAngle.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/LSM303DLM.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/LSM303DLM.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/Timer.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/Timer.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/TinyGPSPlus/TinyGPS++.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/TinyGPSPlus/TinyGPS++.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/Utility.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/Utility.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/VectorFilter.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/VectorFilter.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/WindFilter.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/WindFilter.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/WindStatFilter.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/WindStatFilter.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/filterAlphaBeta.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/filterAlphaBeta.cpp.o"
+  "/home/bryan/mun-sailbot/BeagleCode/SailbotBrain/src/main.cpp" "/home/bryan/mun-sailbot/BeagleCode/build/SailbotBrain/CMakeFiles/SailbotBrain.dir/src/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bryan/mun-sailbot/BeagleCode/build/BeagleUtil/CMakeFiles/BeagleUtil.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../SailbotBrain/include"
+  "../SailbotBrain/include/TinyGPSPlus"
+  "../ncurses/include"
+  "../BeagleUtil/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
