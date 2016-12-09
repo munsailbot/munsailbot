8d6b51c2dc2a0dcea659b45827d025b60e7c7c43
bnuh <bryan@okplus.ca>
2016-12-08 19:35:15 -0500

    Data cleaning and mapping set up

--- graph.R
+++ graph.R
@@ -1,10 +1,18 @@
+# Greater precision required for mapping
 setwd("/Users/bryan/Git/munsailbot")
-data <- read.csv("track.csv", sep=",", header = TRUE)
 
+# Import CSV and delete empty/NA rows
+data <- read.table('track.csv', header = TRUE, sep =',')
+data <- subset(data, Lat!=c("99.99",""),select = c(Lon,Lat))
+tab <- table(data$Lon,data$Lat)
+plot(data,type="o")
+
+# Extrema
 X1 <- min(data["Lon"])
 X2 <- max(data["Lon"])
 Y1 <- min(data["Lat"])
 Y2 <- max(data["Lat"])
 
+# Creating a 100x100 grid
 divx <- abs(X1-X2)/100
 divy <- abs(Y1-Y2)/100
