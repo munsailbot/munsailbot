29b4fcea3fc64bb4996b4286e2565b3c66d49a97
brymel <bryan.melanson@gmail.com>
2017-02-28 17:49:52 -0330

    Conditional point sizes for sailstate

--- Docs/graph.R
+++ Docs/graph.R
@@ -1,22 +1,20 @@
-# Greater precision required for mapping
 library(ggplot2)
 
 setwd("/Users/bryan/Git/munsailbot")
 
 # Import CSV and delete empty/NA rows
+# TODO: Use aes to define evenly spaced X/Y ticks
 data <- read.table('track.csv', header = TRUE, sep =',')
-data <- subset(data, Lat!=c("99.99",""),select = c(Lon,Lat))
-data1 <- matrix(c(47.57531,47.57530,47.57533,47.57534,47.57535,-52.73621,-52.73622,-52.73623,-52.73624,-52.73623), ncol=2)
-tab <- table(data$Lon,data$Lat)
-#plot(data1,type="o",ylab="Lon",xlab="Lat")
-ggplot(data,aes(x=data$Lon, y=data$Lat)) + geom_point(data=data, aes(x=data$Lon, y=data$Lat), color="red") + labs(x="",y="") + geom_path()
+data <- subset(data, Lat!=c("99.99"))
+data <- subset(data, Lon!=c("99.99"))
+state <- c(data$Sailstate)
 
-# Extrema
-X1 <- min(data["Lon"])
-X2 <- max(data["Lon"])
-Y1 <- min(data["Lat"])
-Y2 <- max(data["Lat"])
+ss <- function(x){ifelse(x > 0, 5, 2)}
 
-# Creating a 100x100 grid
-divx <- abs(X1-X2)/100
-divy <- abs(Y1-Y2)/100
+ggplot(data,aes(x=data$Lon, y=data$Lat)) +
+  geom_point(color="red", (aes(size = ss(state)))) +
+  labs(x="",y="") +
+  geom_path() +
+  coord_equal()
+
+#fin
