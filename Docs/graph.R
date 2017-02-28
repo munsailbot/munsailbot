bd5f288e5fec27200c542723e549f9e1f4c6ad00
brymel <bryan.melanson@gmail.com>
2017-02-28 16:13:50 -0330

    Points and lines graphing

--- Docs/graph.R
+++ Docs/graph.R
@@ -1,5 +1,6 @@
 # Greater precision required for mapping
 library(ggplot2)
+
 setwd("/Users/bryan/Git/munsailbot")
 
 # Import CSV and delete empty/NA rows
@@ -7,7 +8,8 @@ data <- read.table('track.csv', header = TRUE, sep =',')
 data <- subset(data, Lat!=c("99.99",""),select = c(Lon,Lat))
 data1 <- matrix(c(47.57531,47.57530,47.57533,47.57534,47.57535,-52.73621,-52.73622,-52.73623,-52.73624,-52.73623), ncol=2)
 tab <- table(data$Lon,data$Lat)
-plot(data1,type="o",ylab="Lon",xlab="Lat")
+#plot(data1,type="o",ylab="Lon",xlab="Lat")
+ggplot(data,aes(x=data$Lon, y=data$Lat)) + geom_point(data=data, aes(x=data$Lon, y=data$Lat), color="red") + labs(x="",y="") + geom_path()
 
 # Extrema
 X1 <- min(data["Lon"])
